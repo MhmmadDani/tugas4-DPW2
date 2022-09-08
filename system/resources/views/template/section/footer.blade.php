8d0e4dd0ad82d509fe77fb6a693fbe87f61373c8
Muhammad Dani <103087509+MhmmadDani@users.noreply.github.com>
2022-09-08 17:14:19 +0700

    Initial commit

---
+++ system/resources/views/template/section/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Muhammad Dani
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 @if(date("Y") > '2022') {{date("Y")}} @endif <a href="AldiKurniawan.com">Admin E-SHOPPER></a>.</strong> All rights reserved.
+  </footer>
